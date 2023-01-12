488fab4999017e6aaf5f9d2d9bf5fb321cdd02f4
seonghwa <ghdtack@ajou.ac.kr>
2023-01-12 14:05:14 +0900

    230112 모각소 끝

---
+++ HongTIL/20230112/react-for-beginners/src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button"
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev +1);
+  console.log('i run all the time');
+  useEffect(() => {
+    console.log("CALL TEH API....");
+  }, []);
+  return (
+    <div>
+      <h1 className={styles.title}>
+        {counter}
+      </h1>
+      <button onClick={onClick}>click me</button>
+      <Button text={"Continue"}/>
+    </div>
+  );
+} 
+
+export default App;
---
+++ HongTIL/20230112/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for i in range(0,n):
+#     for j in range(0, n):
+#         if adj[i][j] == 1:
